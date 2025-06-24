6124064cbea3b72f1d2fdc3098c30f72fec37423
PiGames <root_4rch@proton.me>
2025-06-24 16:20:12 +0300

    everything uploaded

---
+++ README.md
@@ -0,0 +1,15 @@
+# pacostrap
+## pacstrap for offline installation(manuel arch installation)
+
+Hey guys!
+This project is not done yet. So this README wont be long. 
+My goal is using a USB(except archiso USB) to install packages on it than install them from USB. 
+This project is not writing pacstrap from scratch for offline installation, it uses pacstrap with a few options and customised settings.
+
+Like i said, this project have not done yet (it just formats USB) but i will develop it fully within few days. 
+
+Hey, archlinux devs! If you saw my repo, i would be verry happy if you put this software to archiso. 
+
+LICENSE
+
+GNU General Public License version 3 (GPLv3)
---
+++ pacostrap.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <unistd.h>
+using namespace std;
+
+int main(){ //int mainin 1. süslü parantezi
+	cout << "Welcome to pacostrap beta! Which stage are you on?" << endl;
+	cout << "1: Before installation, 2: In archiso." << endl;
+	int stage;
+	cin >> stage;
+	switch(stage){ //adımın(stage) 1. süslü parantezi
+		case 1:{ // stage/case 1 in 1. süslü parantezi
+			       if( getuid() == 0){ // stage/case/if in 1. süslü parantezi
+				       cout << "For offline installation, you will need another USB(except archiso USB.)" <<  endl;
+				       cout << "THIS OPERATION WILL FORMAT AND ERASE ALL YOUR DATA IN USB! ARE YOU SURE?(y(es) or n(o))" << endl;
+				       char formatoption;
+				       cin >> formatoption;
+				       switch(formatoption){ // stage/case/if/formatoptionun 1. süslü paranteezi
+					       case 'y':
+					       case 'Y':{  // stage/case/if/formatoption/case yes'in 1. süslü parantezi
+								cout << "Enter your USB's path.(path is /dev/*your-usb* . example: /dev/sdc)." << endl;
+								cout << "You can learn *your-usb* with lsblk command." << endl;
+								string usbpath;
+								cin >> usbpath;
+								
+								string cmd = "mkfs.ext4 " + usbpath ;
+								int result = system(cmd.c_str());
+								if (result !=0){ //usb formatın/if ilk süslü parantezi
+										 cout <<  "Failed to format USB. Maybe path is wrong or something else." << endl;
+										 return 1;
+								} // usb formatın/if ikinci süslü parantezi
+								 else{ //usb formatın/else ilk süslü parantezi
+									 cout << "USB format: succes!" << endl;
+								 }
+							}
+				       }
+			       }
+			       else{
+				       cout << "You need to be root." << endl;
+			       }
+		       }
+	}
+}
