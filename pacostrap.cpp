91501af6615a6f97b4b3f803d02867759987b169
PiGames <root_4rch@proton.me>
2025-06-25 22:20:18 +0300

    1ST STAGE FULLY COMPLETED!
    pacostrap now:
    can format[usb],
    can mount[usb],
    can install packages[to usb]

--- pacostrap.cpp
+++ pacostrap.cpp
@@ -1,69 +1,111 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <cstdlib>
 #include <unistd.h>
 
 using namespace std;
 
-
-int main(){ //int mainin 1. süslü parantezi
+int main(){
 	if(getuid() == 0){
-	cout << "Welcome to pacostrap beta! Which stage are you on?" << endl;
-	cout << "1: Before installation, 2: In archiso." << endl;
-	int stage;
-	cin >> stage;
-	switch(stage){ //adımın(stage) 1. süslü parantezi
-		case 1:{ // stage/case 1 in 1. süslü parantezi
-				       cout << "For offline installation, you will need another USB(except archiso USB.)" <<  endl;
-				       cout << "THIS OPERATION WILL FORMAT AND ERASE ALL YOUR DATA IN USB! ARE YOU SURE?(y(es) or n(o))" << endl;
-				       char formatoption;
-				       cin >> formatoption;
-				       switch(formatoption){ // stage/case/if/formatoptionun 1. süslü paranteezi
-					       case 'y':
-					       case 'Y':{  // stage/case/if/formatoption/case yes'in 1. süslü parantezi
-								cout << "Enter your USB's path.(path is /dev/*your-usb* .Example: /dev/sdc)." << endl;
-								cout << "You can learn *your-usb* with lsblk command." << endl;
-								string usbpath;
-								cin >> usbpath;
-								cout << "Formatting USB..." << endl;
-								string cmd = "mkfs.ext4 " + usbpath ;
-								int result = system(cmd.c_str());
-								if (result !=0){ //usb formatın/if ilk süslü parantezi
-										 cout <<  "Failed to format USB." << endl;
-										 return 1;
-								} // usb formatın/if ikinci süslü parantezi
-								 else{ //usb formatın/else ilk süslü parantezi
-									 cout << "USB format: succes!" << endl;
-									 cout << "Mounting USB..." << endl;
-									 string mcmd = "mount " + usbpath + " /mnt" ;
-									 int mresult = system(mcmd.c_str());
-									 if (result !=0){
-										 cout << "Failed to mount USB." << endl;
-										 return 1;
-									 }
-									 else{
-										 cout << "Mount: Succes!" << endl;
-										 cout << "Enter the packages that you want to install." << endl;
-										 // sen burayı hallet pacman -Sw falan vardı
-									 }
-								 }
-							}
-							break;
-					       case 'n':
-					       case 'N':{
-								cout << "Have a nice day." << endl;
+		cout << "Welcome to pacostrap beta! Which stage are you on?" << endl;
+		cout << "1: Before installation, 2: In archiso." << endl;
+		int stage;
+		cin >> stage;
+
+		switch(stage){
+			case 1:{
+				cout << "For offline installation, you will need another USB(except archiso USB.)" <<  endl;
+				cout << "THIS OPERATION WILL FORMAT AND ERASE ALL YOUR DATA IN USB! ARE YOU SURE?(y(es) or n(o))" << endl;
+				char formatoption;
+				cin >> formatoption;
+
+				switch(formatoption){
+					case 'y':
+					case 'Y':{
+						cout << "Enter your USB's path.(path is /dev/*your-usb* .Example: /dev/sdc)." << endl;
+						cout << "You can learn *your-usb* with lsblk command." << endl;
+						string usbpath;
+						cin >> usbpath;
+
+						cout << "Formatting USB..." << endl;
+						string cmd = "mkfs.ext4 " + usbpath ;
+						int result = system(cmd.c_str());
+						if (result !=0){
+							cout <<  "Failed to format USB." << endl;
+							return 1;
+						} else{
+							cout << "USB format: success!" << endl;
+							cout << "Mounting USB..." << endl;
+							string mcmd = "mount " + usbpath + " /mnt" ;
+							system("mkdir -p /mnt");  // mount edilecek dizin varsa sorun olmaz
+							int mresult = system(mcmd.c_str());
+							if (mresult !=0){
+								cout << "Failed to mount USB." << endl;
 								return 1;
+							} else {
+								cout << "Mount: Success!" << endl;
+								vector<string> paketler;
+								string input;
+
+								cout << "Enter packages you want to install order by order. Enter 'break' to end installing packages." << endl;
+
+								while (true) {
+									cout << ">> ";
+									getline(cin, input);
+									if (input == "break")
+										break;
+
+									if (input.empty() || input.find_first_not_of(" \t") == string::npos) {
+										cout << "Space is not a package!" << endl;
+										continue;
+									}
+
+									paketler.push_back(input);
+								}
+
+								if (paketler.empty()) {
+									cout << "No packages given." << endl;
+									return 1;
+								}
+
+								// Paket isimlerini tek string'e birleştir
+								string komut = "sudo pacman -Sw --cachedir /mnt --noconfirm";
+								for (const auto& pkg : paketler) {
+									komut += " " + pkg;
+								}
+								int sonuc = system(komut.c_str());
+
+								if (sonuc != 0) {
+									cout << "Paket indirme başarısız." << endl;
+									return 1;
+								}
+
+								cout << "Paketler başarıyla indirildi!" << endl;
+								return 0;
 							}
-							break;
-						   default:{
-							   cout << "This option doesn't exists!" << endl;
-							   return 1;
+
+
+							}
 						}
-						break;
-				       }
-				   }
-	}
-	}
+					}
+					break;
+					case 'n':
+					case 'N':{
+						cout << "Have a nice day." << endl;
+						return 1;
+					}
+					break;
+					default:{
+						cout << "This option doesn't exist!" << endl;
+						return 1;
+					}
+					break;
+				}
+			}
+		}
 	else{
 		cout << "You have to be root." << endl;
 	}
 }
+
