cf48f080edc6f8b3cd2064410f763993f45670c1
PiGames <root_4rch@proton.me>
2025-06-26 12:42:29 +0300

    multiple source codes and .hpp file

---
+++ src/pacostrap.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <unistd.h>
+#include "stages.hpp"
+
+using namespace std;
+
+int main(){
+	if(getuid() == 0){
+		cout << "Welcome to pacostrap beta! Which stage are you on?" << endl;
+		cout << "1: Before installation, 2: In archiso." << endl;
+		int stage;
+		cin >> stage;
+
+		switch(stage){
+			case 1:{
+				firststage();
+				return 0;
+			}
+			default:{
+				cout << "This option doesn't exists!" << endl;
+			}
+		}
+	}
+else{
+	cout << "You have to be root." << endl;
+}
+}
