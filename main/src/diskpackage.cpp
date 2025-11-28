1a8f9f5ea67f2a01992b49e7675b9b02c7f74a2d
npc-gnu <root_4rch@proton.me>
2025-11-28 22:52:25 +0300

    pacstrap operation bug fixed in projectrootdir/main/src/diskpackage.cpp

--- main/src/diskpackage.cpp
+++ main/src/diskpackage.cpp
@@ -2,37 +2,40 @@
 #include <cstdlib> // exit function
 #include <thread> // for animation
 #include "spinner.hpp" // spinner animation
-#include "command.hpp" // my awesome super duper perfect function(joke)
 using namespace std;
 void diskpackage() {
-    cout << "\033[36mPacostrap, now calling pacstrap.\033[0m\n";
-    atomic<bool> animating(true);
-    thread animThread(animatel, std::ref(animating), 100);
-    int cpresult = command("cp /usbmnt/*.zst /mnt/var/cache/pacman/pkg/") ;
-    int forresult = command("for f in /mnt/var/cache/pacman/pkg/*.zst; do pacman -Qp \"$f\" | awk '{print $1}'; done > pkglist.txt");
-    int pacresult = command("pacstrap -K /mnt $(cat pkglist.txt) --cachedir=/mnt/var/cache/pacman/pkg") ;
-    if (cpresult !=0) {
-        animating = false;
-    	animThread.join();
-    	cout << "\n";
-        cout << "\033[31;40mPackage copy operation failed.\033[0m" << endl;
-        exit(1);
-    } else if (forresult !=0) {
-        animating = false;
-    	animThread.join();
-    	cout << "\n";
-        cout << "\033[31;40mTaking package names failed.\033[0m" << endl;
-        exit(1);
-    } else if (pacresult !=0) {
-    	animating = false;
-    	animThread.join();
-    	cout << "\n";
-        cout << "\033[31;40mPacstrap, so pacostrap failed.\033[0m" << endl;
-        exit(1);
-    } else {
-    	animating = false;
-    	animThread.join();
-    	cout << "\n";
-        cout << "\033[32mCongrats! Your offline Arch GNU+Linux installation ended.\033[0m" << endl;
-    } 
+	cout << "\033[36mPacostrap, now calling pacstrap.\033[0m\n";
+	atomic<bool> animating(true);
+	thread animThread(animatel, std::ref(animating), 100);
+	int cpresult = system("cp /usbmnt/*.zst /mnt/var/cache/pacman/pkg/") ;
+	int forresult = system("for f in /mnt/var/cache/pacman/pkg/*.zst; do pacman -Qp \"$f\" | awk '{print $1}'; done > pkglist.txt");
+	int pacresult = system("pacstrap -K /mnt $(cat pkglist.txt) --cachedir=/mnt/var/cache/pacman/pkg") ;
+	/* In case you wondering, why the fuck are you using system() instead of you "awesome" command()?
+	* Because of these are literally scripts, not just some pacman -S so i have to use system (or command() with /bin/sh -c)
+	* I will start using json so i can return back to command but until that time, using system() 
+	*/
+	if (cpresult !=0) {
+		animating = false;
+		animThread.join();
+		cout << "\n";
+		cout << "\033[31;40mPackage copy operation failed.\033[0m" << endl;
+		exit(1);
+	} else if (forresult !=0) {
+		animating = false;
+		animThread.join();
+		cout << "\n";
+		cout << "\033[31;40mTaking package names failed.\033[0m" << endl;
+		exit(1);
+	} else if (pacresult !=0) {
+		animating = false;
+		animThread.join();
+		cout << "\n";
+		cout << "\033[31;40mPacstrap, so pacostrap failed.\033[0m" << endl;
+		exit(1);
+	} else {
+		animating = false;
+		animThread.join();
+		cout << "\n";
+		cout << "\033[32mCongrats! Your offline Arch GNU+Linux installation ended.\033[0m" << endl;
+	} 
 }
